5a4771d10d0736efcdba972659d4d6eeae89ca8b
Kenneth <kennethrogers.kr@gmail.com>
2018-02-19 14:38:42 +0100

    feat: Add Joke class for API query
    Add Joke class to be used to store data retrieved from API response.

---
+++ app/src/main/java/com/roguekingapps/jokesby/data/network/Joke.java
@@ -0,0 +1,57 @@
+package com.roguekingapps.jokesby.data.network;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Joke {
+
+    private String id;
+    private String title;
+
+    @SerializedName("selftext")
+    private String body;
+
+    @SerializedName("author")
+    private String user;
+
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
