4b4d7298275307c4df4fea56bbb334835b3489de
Kenneth <kennethrogers.kr@gmail.com>
2018-02-20 15:02:21 +0100

    feat: Reduce score query parameter
    Reduce score query parameter to yield more results in response.

--- app/src/main/java/com/roguekingapps/jokesby/data/network/JokeApi.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/network/JokeApi.java
@@ -1,11 +1,13 @@
 package com.roguekingapps.jokesby.data.network;
 
+import com.roguekingapps.jokesby.data.network.model.JokeContainer;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface JokeApi {
 
-    @GET("reddit/search/submission/?subreddit=jokes&stickied=false&size=500&score=>1000&sort=desc")
+    @GET("reddit/search/submission/?subreddit=jokes&stickied=false&size=500&score=>50&sort=desc")
     Call<JokeContainer> loadJokes(@Query("before") String daysBefore, @Query("after") String daysAfter);
 }
