4b8559b0d7eb4562a86689e8a0a12668b674e96d
Kenneth <kennethrogers.kr@gmail.com>
2018-02-20 22:14:02 +0100

    feat: Provide MainView
    Provide MainView for communication with presenter.

--- app/src/main/java/com/roguekingapps/jokesby/di/module/ActivityModule.java
+++ app/src/main/java/com/roguekingapps/jokesby/di/module/ActivityModule.java
@@ -1,13 +1,26 @@
 package com.roguekingapps.jokesby.di.module;
 
+import com.roguekingapps.jokesby.ui.main.MainActivity;
 import com.roguekingapps.jokesby.ui.main.MainPresenter;
 import com.roguekingapps.jokesby.ui.main.MainPresenterImpl;
+import com.roguekingapps.jokesby.ui.main.MainView;
 
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class ActivityModule {
+
+    private MainActivity activity;
+
+    public ActivityModule(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    @Provides
+    MainView provideMainView() {
+        return activity;
+    }
 
     @Provides
     MainPresenter provideMainPresenter(MainPresenterImpl mainPresenter) {
