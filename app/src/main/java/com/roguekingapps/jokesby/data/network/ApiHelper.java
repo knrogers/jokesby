4ba257e0a587909ebd640af373b4bf8298ecc4d3
Kenneth <kennethrogers.kr@gmail.com>
2018-02-20 14:59:45 +0100

    feat: Make call to load jokes

--- app/src/main/java/com/roguekingapps/jokesby/data/network/ApiHelper.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/network/ApiHelper.java
@@ -1,9 +1,11 @@
 package com.roguekingapps.jokesby.data.network;
 
-import com.google.gson.Gson;
+import com.roguekingapps.jokesby.data.network.model.JokeContainer;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import retrofit2.Call;
 
 /**
  * Accesses the API and loads jokes.
@@ -11,14 +13,18 @@ import javax.inject.Singleton;
 @Singleton
 public class ApiHelper implements BaseApiHelper {
 
-    private Gson gson;
+    private ApiResponse apiResponse;
+    private JokeApi jokeApi;
 
     @Inject
-    ApiHelper(Gson gson) {
-        this.gson = gson;
+    ApiHelper(ApiResponse apiResponse, JokeApi jokeApi) {
+        this.apiResponse = apiResponse;
+        this.jokeApi = jokeApi;
     }
 
     @Override
     public void loadJokes() {
+        Call<JokeContainer> call = jokeApi.loadJokes("11d", "12d");
+        call.enqueue(apiResponse);
     }
 }
