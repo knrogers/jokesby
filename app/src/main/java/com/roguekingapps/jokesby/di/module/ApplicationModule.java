713598f4f082bce0ad606a5c94b82479a88344c8
Kenneth <kennethrogers.kr@gmail.com>
2018-02-20 14:55:39 +0100

    refactor: Provide JokeApi and ApiResponse for ApiHelper

--- app/src/main/java/com/roguekingapps/jokesby/di/module/ApplicationModule.java
+++ app/src/main/java/com/roguekingapps/jokesby/di/module/ApplicationModule.java
@@ -5,12 +5,20 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.roguekingapps.jokesby.R;
+import com.roguekingapps.jokesby.data.network.ApiResponse;
+import com.roguekingapps.jokesby.data.network.JokeApi;
 import com.roguekingapps.jokesby.di.ApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class ApplicationModule {
@@ -34,9 +42,37 @@ public class ApplicationModule {
 
     @Provides
     @Singleton
-    Gson provideGson() {
+    ApiResponse provideApiResponse() {
+        return new ApiResponse();
+    }
+
+    @Provides
+    @Singleton
+    JokeApi provideJokeApi() {
+        return provideRetrofit().create(JokeApi.class);
+    }
+
+    @Singleton
+    private Retrofit provideRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl(application.getString(R.string.base_api_url))
+                .addConverterFactory(GsonConverterFactory.create(provideGson()))
+                .client(provideOkHttpClient())
+                .build();
+    }
+
+    @Singleton
+    private Gson provideGson() {
         return new GsonBuilder()
                 .setLenient()
                 .create();
+    }
+
+    @Singleton
+    private OkHttpClient provideOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .build();
     }
 }
