92f288ec3d2838d7fd64a5d9b47c1c8001326c8b
Kenneth <kennethrogers.kr@gmail.com>
2018-02-21 18:58:54 +0100

    refactor: Create DataManager
    Create DataManager as an abstract layer between presenters and helper
    classes.

---
+++ app/src/main/java/com/roguekingapps/jokesby/data/DataManager.java
@@ -0,0 +1,8 @@
+package com.roguekingapps.jokesby.data;
+
+import com.roguekingapps.jokesby.ui.main.MainPresenter;
+
+public interface DataManager {
+
+    void loadJokes(MainPresenter mainPresenter);
+}
---
+++ app/src/main/java/com/roguekingapps/jokesby/data/DataManagerImpl.java
@@ -0,0 +1,41 @@
+package com.roguekingapps.jokesby.data;
+
+import com.roguekingapps.jokesby.data.network.ApiHelper;
+import com.roguekingapps.jokesby.data.network.model.Joke;
+import com.roguekingapps.jokesby.data.network.model.JokeContainer;
+import com.roguekingapps.jokesby.ui.main.MainPresenter;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.functions.Consumer;
+
+/**
+ * Communicates between the data helper classes, i.e {@link ApiHelper}, and the presenters.
+ */
+public class DataManagerImpl implements DataManager {
+
+    private ApiHelper apiHelper;
+
+    @Inject
+    DataManagerImpl(ApiHelper apiHelper) {
+        this.apiHelper = apiHelper;
+    }
+
+    @Override
+    public void loadJokes(final MainPresenter presenter) {
+        Consumer<JokeContainer> jokeConsumer = new Consumer<JokeContainer>() {
+            @Override
+            public void accept(JokeContainer jokeContainer) throws Exception {
+                if (jokeContainer != null) {
+                    List<Joke> jokes = jokeContainer.getJokes();
+                    if (jokes != null && !jokes.isEmpty()) {
+                        presenter.showJokes(jokes);
+                    }
+                }
+            }
+        };
+        apiHelper.loadJokes(jokeConsumer);
+    }
+}
