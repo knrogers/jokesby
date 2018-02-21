d12d3464e762797c85a2307c795373f9639a6540
Kenneth <kennethrogers.kr@gmail.com>
2018-02-21 19:27:00 +0100

    refactor: Update GET to return Observable
    Update GET to return Observable as part of RxJava integration.

--- app/src/main/java/com/roguekingapps/jokesby/data/network/JokeApi.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/network/JokeApi.java
@@ -2,12 +2,12 @@ package com.roguekingapps.jokesby.data.network;
 
 import com.roguekingapps.jokesby.data.network.model.JokeContainer;
 
-import retrofit2.Call;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface JokeApi {
 
     @GET("reddit/search/submission/?subreddit=jokes&stickied=false&size=500&score=>50&sort=desc")
-    Call<JokeContainer> loadJokes(@Query("before") String daysBefore, @Query("after") String daysAfter);
+    Observable<JokeContainer> loadJokes(@Query("before") String daysBefore, @Query("after") String daysAfter);
 }
