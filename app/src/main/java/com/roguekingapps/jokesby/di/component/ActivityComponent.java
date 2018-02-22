230b4e90d03ee15f064bf5570cc6cc1b0b300079
Kenneth <kennethrogers.kr@gmail.com>
2018-02-22 17:30:44 +0100

    refactor: Remove unused getter from module

--- app/src/main/java/com/roguekingapps/jokesby/di/component/ActivityComponent.java
+++ app/src/main/java/com/roguekingapps/jokesby/di/component/ActivityComponent.java
@@ -3,7 +3,6 @@ package com.roguekingapps.jokesby.di.component;
 import com.roguekingapps.jokesby.di.module.ActivityModule;
 import com.roguekingapps.jokesby.ui.main.MainActivity;
 import com.roguekingapps.jokesby.di.PerActivity;
-import com.roguekingapps.jokesby.ui.main.MainPresenter;
 
 import dagger.Component;
 
@@ -12,6 +11,4 @@ import dagger.Component;
 public interface ActivityComponent {
 
     void inject(MainActivity activity);
-
-    MainPresenter getMainPresenter();
 }
