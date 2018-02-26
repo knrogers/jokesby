47d244180e8aea6c6bebcc41b1e378baa750a326
Kenneth <kennethrogers.kr@gmail.com>
2018-02-26 10:07:40 +0100

    feat: Add favourite icon to toolbar

--- app/src/main/java/com/roguekingapps/jokesby/ui/detail/DetailActivity.java
+++ app/src/main/java/com/roguekingapps/jokesby/ui/detail/DetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.roguekingapps.jokesby.R;
 import com.roguekingapps.jokesby.data.network.model.Joke;
@@ -30,5 +32,25 @@ public class DetailActivity extends AppCompatActivity {
         if (joke != null) {
             binding.detailTextViewTitle.setText(joke.getTitle());
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_favourite:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
