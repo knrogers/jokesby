eebd6f5c987089c19224ccb746ef37795375db3b
Kenneth <kennethrogers.kr@gmail.com>
2018-02-27 10:15:29 +0100

    feat: Implement insertJoke method
    Implement insertJoke method to add joke to database.

--- app/src/main/java/com/roguekingapps/jokesby/data/database/DatabaseHelper.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/database/DatabaseHelper.java
@@ -1,8 +1,15 @@
 package com.roguekingapps.jokesby.data.database;
 
+import android.net.Uri;
+
+import com.roguekingapps.jokesby.data.network.model.Joke;
+
+import io.reactivex.Observer;
 import io.reactivex.functions.Consumer;
 
 public interface DatabaseHelper {
 
-    void deleteJoke(Consumer<Integer> rowsDeletedConsumer, String apiId);
+    void deleteJoke(Consumer<Integer> deleteFavouriteConsumer, String apiId);
+
+    void insertJoke(Observer<Uri> insertFavouriteObserver, Joke joke);
 }
--- app/src/main/java/com/roguekingapps/jokesby/data/database/DatabaseHelperImpl.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/database/DatabaseHelperImpl.java
@@ -1,8 +1,10 @@
 package com.roguekingapps.jokesby.data.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.net.Uri;
 
+import com.roguekingapps.jokesby.data.database.JokeContract.JokeEntry;
 import com.roguekingapps.jokesby.data.network.model.Joke;
 import com.roguekingapps.jokesby.di.ApplicationContext;
 
@@ -10,6 +12,9 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
@@ -28,16 +33,41 @@ public class DatabaseHelperImpl implements DatabaseHelper {
     }
 
     @Override
-    public void deleteJoke(Consumer<Integer> rowsDeletedConsumer, String apiId) {
-        Uri uri = JokeContract.JokeEntry.CONTENT_URI
+    public void deleteJoke(Consumer<Integer> deleteFavouriteConsumer, String apiId) {
+        Uri uri = JokeEntry.CONTENT_URI
                 .buildUpon()
-                .appendPath(JokeContract.JokeEntry.COLUMN_API_ID)
+                .appendPath(JokeEntry.COLUMN_API_ID)
                 .appendPath(apiId)
                 .build();
 
         Observable<Integer> observable = Observable.just(context.getContentResolver().delete(uri, null, null));
         observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(rowsDeletedConsumer);
+                .subscribe(deleteFavouriteConsumer);
+    }
+
+    @Override
+    public void insertJoke(Observer<Uri> insertFavouriteConsumer, Joke joke) {
+        final ContentValues contentValues = new ContentValues();
+        contentValues.put(JokeEntry.COLUMN_API_ID, joke.getId());
+        contentValues.put(JokeEntry.COLUMN_TITLE, joke.getTitle());
+        contentValues.put(JokeEntry.COLUMN_BODY, joke.getBody());
+        contentValues.put(JokeEntry.COLUMN_USER, joke.getUser());
+        contentValues.put(JokeEntry.COLUMN_URL, joke.getUrl());
+
+        Observable<Uri> observable = Observable.create(new ObservableOnSubscribe<Uri>() {
+            @Override
+            public void subscribe(ObservableEmitter<Uri> emitter) throws Exception {
+                Uri uri = context.getContentResolver().insert(JokeEntry.CONTENT_URI, contentValues);
+                if (uri == null) {
+                    emitter.onError(new Exception("Uri returned from insert was null."));
+                    return;
+                }
+                emitter.onNext(uri);
+            }
+        });
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(insertFavouriteConsumer);
     }
 }
