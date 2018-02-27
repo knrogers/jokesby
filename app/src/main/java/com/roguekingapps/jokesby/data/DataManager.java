8db02d076e07cb87635b910da1b00af946353aa4
Kenneth <kennethrogers.kr@gmail.com>
2018-02-27 10:18:38 +0100

    feat: Implement insertJoke in DataManager
    Implement insertJoke in DataManager to create an Observer and
    communicate with presenter when insert into database has been completed.

--- app/src/main/java/com/roguekingapps/jokesby/data/DataManager.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/DataManager.java
@@ -1,10 +1,12 @@
 package com.roguekingapps.jokesby.data;
 
+import com.roguekingapps.jokesby.data.network.model.Joke;
 import com.roguekingapps.jokesby.ui.detail.DetailPresenter;
 import com.roguekingapps.jokesby.ui.main.MainPresenter;
 
 public interface DataManager {
 
     void loadJokes(MainPresenter mainPresenter);
-    void updateJoke(DetailPresenter detailPresenter, String apiId);
+
+    void updateJoke(DetailPresenter detailPresenter, Joke joke);
 }
--- app/src/main/java/com/roguekingapps/jokesby/data/DataManagerImpl.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/DataManagerImpl.java
@@ -1,9 +1,15 @@
 package com.roguekingapps.jokesby.data;
 
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.roguekingapps.jokesby.R;
 import com.roguekingapps.jokesby.data.database.DatabaseHelper;
 import com.roguekingapps.jokesby.data.network.ApiHelper;
 import com.roguekingapps.jokesby.data.network.model.Joke;
 import com.roguekingapps.jokesby.data.network.model.JokeContainer;
+import com.roguekingapps.jokesby.di.ApplicationContext;
 import com.roguekingapps.jokesby.ui.detail.DetailPresenter;
 import com.roguekingapps.jokesby.ui.main.MainPresenter;
 
@@ -12,6 +18,8 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
 /**
@@ -20,11 +28,15 @@ import io.reactivex.functions.Consumer;
 @Singleton
 public class DataManagerImpl implements DataManager {
 
+    private Context context;
     private ApiHelper apiHelper;
     private DatabaseHelper databaseHelper;
 
     @Inject
-    DataManagerImpl(ApiHelper apiHelper, DatabaseHelper databaseHelper) {
+    DataManagerImpl(@ApplicationContext Context context,
+                    ApiHelper apiHelper,
+                    DatabaseHelper databaseHelper) {
+        this.context = context;
         this.apiHelper = apiHelper;
         this.databaseHelper = databaseHelper;
     }
@@ -46,17 +58,43 @@ public class DataManagerImpl implements DataManager {
     }
 
     @Override
-    public void updateJoke(final DetailPresenter presenter, String apiId) {
+    public void updateJoke(final DetailPresenter presenter, final Joke joke) {
         Consumer<Integer> deleteFavouriteConsumer = new Consumer<Integer>() {
             @Override
             public void accept(Integer rowsDeleted) throws Exception {
-                boolean favourite = false;
                 if (rowsDeleted == null || rowsDeleted == 0) {
+                    insertJoke(presenter, joke);
                     return;
                 }
-                presenter.updateFavouriteIcon(favourite);
+                presenter.updateFavouriteIcon(false);
             }
         };
-        databaseHelper.deleteJoke(deleteFavouriteConsumer, apiId);
+        databaseHelper.deleteJoke(deleteFavouriteConsumer, joke.getId());
+    }
+
+    private void insertJoke(final DetailPresenter presenter, final Joke joke) {
+        Observer<Uri> insertFavouriteConsumer = new Observer<Uri>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Uri uri) {
+                presenter.updateFavouriteIcon(uri != null);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(DataManagerImpl.class.getSimpleName(), e.getMessage(), e);
+                presenter.showError(context.getResources().getString(R.string.error_add_favourite));
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        databaseHelper.insertJoke(insertFavouriteConsumer, joke);
     }
 }
