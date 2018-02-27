3088e3a80422c832450d377b9904387c8a511c6f
Kenneth <kennethrogers.kr@gmail.com>
2018-02-27 11:48:13 +0100

    feat: Implement insert method
    Implement insert into database method.

--- app/src/main/java/com/roguekingapps/jokesby/data/database/JokeContentProvider.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/database/JokeContentProvider.java
@@ -1,16 +1,23 @@
 package com.roguekingapps.jokesby.data.database;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.roguekingapps.jokesby.R;
+import com.roguekingapps.jokesby.data.database.JokeContract.JokeEntry;
+import com.roguekingapps.jokesby.di.Favourites;
+import com.roguekingapps.jokesby.di.FavouritesWithApiId;
 import com.roguekingapps.jokesby.di.component.DaggerDatabaseComponent;
 import com.roguekingapps.jokesby.di.component.DatabaseComponent;
+import com.roguekingapps.jokesby.di.module.DatabaseModule;
 
 import javax.inject.Inject;
 
@@ -25,10 +32,20 @@ public class JokeContentProvider extends ContentProvider {
     @Inject
     UriMatcher uriMatcher;
 
+    @Inject
+    DatabaseOpenHelper databaseOpenHelper;
+
+    @Inject
+    @Favourites
+    int favourites;
+
+    @Inject
+    @FavouritesWithApiId
+    int favouritesWithApiId;
 
     @Override
     public boolean onCreate() {
-        getDatabaseComponent();
+        getDatabaseComponent().inject(this);
         return true;
     }
 
@@ -49,7 +66,28 @@ public class JokeContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+        Context context = getContext();
+        if (context == null) {
+            throw new NullPointerException(INVALID_CONTEXT_VALUE);
+        }
+
+        int match = uriMatcher.match(uri);
+        Uri responseUri;
+        if (match == favourites) {
+            long id = databaseOpenHelper.getWritableDatabase()
+                    .insert(JokeEntry.TABLE_NAME, null, contentValues);
+
+            if (id > 0) {
+                responseUri = ContentUris.withAppendedId(JokeEntry.CONTENT_URI, id);
+            } else {
+                throw new SQLException(context.getString(R.string.insert_row_failed) + uri);
+            }
+        } else {
+            throw new UnsupportedOperationException(context.getString(R.string.unknown_uri) + uri);
+        }
+        context.getContentResolver().notifyChange(responseUri, null);
+
+        return responseUri;
     }
 
     @Override
@@ -76,6 +114,7 @@ public class JokeContentProvider extends ContentProvider {
     public DatabaseComponent getDatabaseComponent() {
         if (databaseComponent == null) {
             databaseComponent = DaggerDatabaseComponent.builder()
+                    .databaseModule(new DatabaseModule(getContext()))
                     .build();
         }
         return databaseComponent;
