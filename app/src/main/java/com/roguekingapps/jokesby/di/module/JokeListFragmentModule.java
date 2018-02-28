9c9a8f70f64f7cc7c333e8923c2d70e63ef48e25
Kenneth <kennethrogers.kr@gmail.com>
2018-02-28 12:21:18 +0100

    feat: Create module for fragment dependency injection

---
+++ app/src/main/java/com/roguekingapps/jokesby/di/module/JokeListFragmentModule.java
@@ -0,0 +1,22 @@
+package com.roguekingapps.jokesby.di.module;
+
+import com.roguekingapps.jokesby.ui.adapter.ListAdapter;
+import com.roguekingapps.jokesby.ui.main.fragment.JokeListFragment;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class JokeListFragmentModule {
+
+    private JokeListFragment jokeListFragment;
+
+    public JokeListFragmentModule(JokeListFragment jokeListFragment) {
+        this.jokeListFragment = jokeListFragment;
+    }
+
+    @Provides
+    ListAdapter provideListAdapter() {
+        return new ListAdapter(jokeListFragment.getContext(), jokeListFragment);
+    }
+}
