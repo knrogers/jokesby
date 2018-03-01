cbee59188b5f072dd56ff21803d1a89c825f9e88
Kenneth <kennethrogers.kr@gmail.com>
2018-03-01 14:53:08 +0100

    refactor: Rename method for clarification

--- app/src/main/java/com/roguekingapps/jokesby/di/module/ApplicationModule.java
+++ app/src/main/java/com/roguekingapps/jokesby/di/module/ApplicationModule.java
@@ -28,7 +28,7 @@ public class ApplicationModule {
 
     @Provides
     @ApplicationContext
-    Context provideContext() {
+    Context provideApplicationContext() {
         return application;
     }
 
