2eb1c2592db63ec1efe957cc447a92e202bb520a
Kenneth <kennethrogers.kr@gmail.com>
2018-03-07 20:59:36 +0100

    feat: Add provides rated id method for UriMatcher
    Add provides rated id method for UriMatcher and add provides invalid
    context string method.

--- app/src/main/java/com/roguekingapps/jokesby/di/module/DatabaseModule.java
+++ app/src/main/java/com/roguekingapps/jokesby/di/module/DatabaseModule.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.UriMatcher;
 
 import com.roguekingapps.jokesby.data.database.JokeContract;
+import com.roguekingapps.jokesby.di.DatabaseName;
 import com.roguekingapps.jokesby.di.DatabaseVersion;
 import com.roguekingapps.jokesby.di.Favourites;
+import com.roguekingapps.jokesby.di.InvalidContextString;
+import com.roguekingapps.jokesby.di.Rated;
 
 import javax.inject.Singleton;
 
@@ -27,20 +30,7 @@ public class DatabaseModule {
     }
 
     @Provides
-    @Singleton
-    UriMatcher provideUriMatcher() {
-        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMatcher.addURI(JokeContract.AUTHORITY, JokeContract.PATH_FAVOURITES, provideFavourites());
-        return uriMatcher;
-    }
-
-    @Provides
-    @Favourites
-    int provideFavourites() {
-        return 100;
-    }
-    
-    @Provides
+    @DatabaseName
     String provideDatabaseName() {
         return "jokesby.db";
     }
@@ -49,5 +39,32 @@ public class DatabaseModule {
     @DatabaseVersion
     int provideDatabaseVersion() {
         return 1;
+    }
+
+    @Provides
+    @InvalidContextString
+    String provideInvalidContextString() {
+        return "Invalid context value: context is null.";
+    }
+
+    @Provides
+    @Singleton
+    UriMatcher provideUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(JokeContract.AUTHORITY, JokeContract.PATH_FAVOURITES, provideFavourites());
+        uriMatcher.addURI(JokeContract.AUTHORITY, JokeContract.PATH_RATED, provideRated());
+        return uriMatcher;
+    }
+
+    @Provides
+    @Favourites
+    int provideFavourites() {
+        return 100;
+    }
+
+    @Provides
+    @Rated
+    int provideRated() {
+        return 200;
     }
 }
