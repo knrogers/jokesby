573a18766c12089a9fc9c22baeaba56e6aaf1e26
Kenneth <kennethrogers.kr@gmail.com>
2018-03-07 21:00:14 +0100

    feat: Add SQL creation statement for rated table

--- app/src/main/java/com/roguekingapps/jokesby/data/database/DatabaseOpenHelper.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/database/DatabaseOpenHelper.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.roguekingapps.jokesby.data.database.JokeContract.FavouriteEntry;
+import com.roguekingapps.jokesby.data.database.JokeContract.RatedEntry;
+import com.roguekingapps.jokesby.di.DatabaseName;
 import com.roguekingapps.jokesby.di.DatabaseVersion;
 
 import javax.inject.Inject;
@@ -14,18 +16,18 @@ import javax.inject.Singleton;
  * Manages database creation and version management.
  */
 @Singleton
-public class FavouritesOpenHelper extends SQLiteOpenHelper {
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
 
     @Inject
-    FavouritesOpenHelper(Context context,
-                         String databaseName,
-                         @DatabaseVersion int databaseVersion) {
+    DatabaseOpenHelper(Context context,
+                       @DatabaseName String databaseName,
+                       @DatabaseVersion int databaseVersion) {
         super(context, databaseName, null, databaseVersion);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sqlCreateDatabase = "CREATE TABLE " +
+        String sqlCreateFavourites = "CREATE TABLE " +
                 FavouriteEntry.TABLE_NAME + " (" +
                 FavouriteEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 FavouriteEntry.COLUMN_API_ID + " TEXT NOT NULL, " +
@@ -34,7 +36,19 @@ public class FavouritesOpenHelper extends SQLiteOpenHelper {
                 FavouriteEntry.COLUMN_USER + " TEXT NOT NULL, " +
                 FavouriteEntry.COLUMN_URL + " TEXT NOT NULL " +
                 ");";
-        sqLiteDatabase.execSQL(sqlCreateDatabase);
+        sqLiteDatabase.execSQL(sqlCreateFavourites);
+
+        String sqlCreateRated = "CREATE TABLE " +
+                RatedEntry.TABLE_NAME + " (" +
+                RatedEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                RatedEntry.COLUMN_API_ID + " TEXT NOT NULL, " +
+                RatedEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                RatedEntry.COLUMN_BODY + " TEXT NOT NULL, " +
+                RatedEntry.COLUMN_USER + " TEXT NOT NULL, " +
+                RatedEntry.COLUMN_URL + " TEXT NOT NULL, " +
+                RatedEntry.COLUMN_RATING + " TEXT NOT NULL " +
+                ");";
+        sqLiteDatabase.execSQL(sqlCreateRated);
     }
 
     @Override
