62e340fe05be3f87b2c40b98ea0c998c5500ab01
Kenneth <kennethrogers.kr@gmail.com>
2018-03-07 20:53:38 +0100

    feat: Create entry for rated table

--- app/src/main/java/com/roguekingapps/jokesby/data/database/JokeContract.java
+++ app/src/main/java/com/roguekingapps/jokesby/data/database/JokeContract.java
@@ -11,6 +11,7 @@ public final class JokeContract  {
     public static final String AUTHORITY = "com.roguekingapps.jokesby";
     static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
     public static final String PATH_FAVOURITES = "favourites";
+    public static final String PATH_RATED = "rated";
 
     private JokeContract() {
 
@@ -24,6 +25,17 @@ public final class JokeContract  {
         public static final String COLUMN_BODY = "body";
         public static final String COLUMN_USER = "user";
         public static final String COLUMN_URL = "url";
+    }
+
+    public static final class RatedEntry implements BaseColumns {
+        static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_RATED).build();
+        static final String TABLE_NAME = "rated";
+        static final String COLUMN_API_ID = "api_id";
+        static final String COLUMN_TITLE = "title";
+        static final String COLUMN_BODY = "body";
+        static final String COLUMN_USER = "user";
+        static final String COLUMN_URL = "url";
+        static final String COLUMN_RATING = "rating";
     }
 }
 
