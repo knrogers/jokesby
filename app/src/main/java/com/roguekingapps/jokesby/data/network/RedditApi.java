e2f805dd050872d78f05fbda468310f110eee360
Kenneth <kennethrogers.kr@gmail.com>
2018-03-12 14:53:55 +0100

    feat: Add Reddit API interface

---
+++ app/src/main/java/com/roguekingapps/jokesby/data/network/RedditApi.java
@@ -0,0 +1,16 @@
+package com.roguekingapps.jokesby.data.network;
+
+import com.roguekingapps.jokesby.data.model.RedditRoot;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RedditApi {
+
+    @GET("r/Jokes/hot.json?limit=50&count=50")
+    Observable<RedditRoot> loadInitialHotJokes();
+
+    @GET("r/Jokes/hot.json?limit=50&count=50")
+    Observable<RedditRoot> loadHotJokes(@Query("after") String after);
+}
